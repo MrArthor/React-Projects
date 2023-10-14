86b7513333d520936a326aaa64705c6e37ecd460
Vansh Sachdev <vksvs25@gmail.com>
2023-10-14 19:52:50 +0530

    Update

--- Project-1 To-Do List/src/components/Expenses/ExpenseItem.js
+++ Project-1 To-Do List/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
-import React from 'react';
+/** @format */
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React, { useState }from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+ const updateTitleHandler=()=> {
+  console.log("Clicked");
+  setTitle("Updated!");
+}
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />{" "}
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
+        <h2> {title} </h2> <div className='expense-item__price'> $ {props.amount} </div>
+        <button onClick={updateTitleHandler}   >Update</button>
+      </div>{" "}
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
